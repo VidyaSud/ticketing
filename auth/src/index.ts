fd1ec4c615a0aa0edc7ab5ea924f1082f0b2bc89
Charushi Sudhakar <charushisudhakar@Charushis-MacBook-Pro.local>
2020-10-25 18:07:27 -0400

    message in startup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Starting up.... Message");
+  console.log("Message on Index page");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY is not defined.");
   }
